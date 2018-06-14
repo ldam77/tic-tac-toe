41afd0d2411e3a4db5a2dd28305d0b62610f1afe
Anousone  Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-06-14 14:36:01 -0700

    Winning conditions check works.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,62 @@ function Player (player) {
   this.pieceLocations = [];
 }
 
-var playerOne = new Player("player1");
-var playerTwo = new Player("player2");
-var playerCpu = new Player("playerCpu");
+var playerOne = new Player("Player ONE");
+var playerTwo = new Player("Player TWO");
+var playerCpu = new Player("Computer");
+var counter = 0;
+var imgX = 'img/x.png';
+var imgO = 'img/check.png';
+var winCondition = [["1a", "1b", "1c"], ["2a", "2b", "2c"], ["3a", "3b", "3c"], ["1a", "2a", "3a"], ["1b", "2b", "3b"], ["1c", "2c", "3c"], ["1a", "2b", "3c"], ["1c", "2b", "3a"]];
 
-
-function changeImage(id){
-  debugger;
+function changeImage(id, img){
   var location = id+"pic";
-  document.getElementById(location).src = 'img/x.png';
+  document.getElementById(location).src = img;
 }
 
-function getId(id) {
-  var myId = id;
-  changeImage(id);
+function playGame(id) {
+  counter +=1;
+  if (counter%2){
+    changeImage(id,imgX);
+    playerOne.pieceLocations.push(id);
+  } else {
+    changeImage(id,imgO);
+    playerTwo.pieceLocations.push(id);
+  };
+
+  var p1Count = 0;
+  var p2Count = 0;
+  winCondition.forEach(function(winningArray){
+    p1Count = 0;
+    p2Count = 0;
+    winningArray.forEach(function(location){
+      if (playerOne.pieceLocations.indexOf(location) > -1){
+        p1Count += 1;
+      };
+      if (playerTwo.pieceLocations.indexOf(location) > -1){
+        p2Count += 1;
+      };
+    });
+    if(p1Count === 3){
+      $("#winner").text(playerOne.name);
+    } else if (p2Count === 3){
+      $("#winner").text(playerTwo.name);
+    };
+  });
+
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)+1);
+}
 
-
+// Player.prototype.playGame = function (){
+  // var imgX = 'img/x.png';
+  // var imgO = 'img/check.png';
+//   for (i=1, 1 < 10, i++) {
+//
+//   }
+// };
 
 // user interface
 $(document).ready(function(){
@@ -31,7 +69,4 @@ $(document).ready(function(){
 
 
   });
-
-
-
 });
