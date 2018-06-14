037a67fa4c34d499a6d803fc71657b47eba4dee5
Anousone  Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-06-14 15:39:22 -0700

    Add draw condition and winning bg.

--- js/scripts.js
+++ js/scripts.js
@@ -8,45 +8,62 @@ var playerOne = new Player("Player ONE");
 var playerTwo = new Player("Player TWO");
 var playerCpu = new Player("Computer");
 var counter = 0;
-var imgX = 'img/x.png';
-var imgO = 'img/check.png';
-var winCondition = [["1a", "1b", "1c"], ["2a", "2b", "2c"], ["3a", "3b", "3c"], ["1a", "2a", "3a"], ["1b", "2b", "3b"], ["1c", "2c", "3c"], ["1a", "2b", "3c"], ["1c", "2b", "3a"]];
-
+const imgX = 'img/x.png';
+const imgO = 'img/check.png';
+const winCondition = [["1a", "1b", "1c"], ["2a", "2b", "2c"], ["3a", "3b", "3c"], ["1a", "2a", "3a"], ["1b", "2b", "3b"], ["1c", "2c", "3c"], ["1a", "2b", "3c"], ["1c", "2b", "3a"]];
+var playArea = ['1a', '2a', '3a', '1b', '2b', '3b', '1c', '2c', '3c'];
+var gameOver = false;
 function changeImage(id, img){
   var location = id+"pic";
   document.getElementById(location).src = img;
 }
 
-function playGame(id) {
-  counter +=1;
-  if (counter%2){
-    changeImage(id,imgX);
-    playerOne.pieceLocations.push(id);
-  } else {
-    changeImage(id,imgO);
-    playerTwo.pieceLocations.push(id);
-  };
+function flashScreen(){
+  $('.row').fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn();
+}
 
-  var p1Count = 0;
-  var p2Count = 0;
-  winCondition.forEach(function(winningArray){
-    p1Count = 0;
-    p2Count = 0;
-    winningArray.forEach(function(location){
-      if (playerOne.pieceLocations.indexOf(location) > -1){
-        p1Count += 1;
-      };
-      if (playerTwo.pieceLocations.indexOf(location) > -1){
-        p2Count += 1;
+function playGame(id) {
+
+  if(playArea.indexOf(id) >= 0){
+    counter +=1;
+    playArea.splice(playArea.indexOf(id), 1);
+
+    if (counter%2){
+      changeImage(id,imgX);
+      playerOne.pieceLocations.push(id);
+    } else {
+      changeImage(id,imgO);
+      playerTwo.pieceLocations.push(id);
+    };
+
+    var p1Count = 0;
+    var p2Count = 0;
+    winCondition.forEach(function(winningArray){
+      p1Count = 0;
+      p2Count = 0;
+      winningArray.forEach(function(location){
+        if (playerOne.pieceLocations.indexOf(location) > -1){
+          p1Count += 1;
+        };
+        if (playerTwo.pieceLocations.indexOf(location) > -1){
+          p2Count += 1;
+        };
+      });
+      if(p1Count === 3){
+        $("#winner").text(playerOne.name);
+        gameOver = true;
+        flashScreen();
+        $('body').toggleClass('win');
+      } else if (p2Count === 3){
+        $("#winner").text(playerTwo.name);
+        gameOver = true;
+        flashScreen();
       };
     });
-    if(p1Count === 3){
-      $("#winner").text(playerOne.name);
-    } else if (p2Count === 3){
-      $("#winner").text(playerTwo.name);
-    };
-  });
-
+  };
+  if(!gameOver && counter === 9){
+    $("#winner").text("Nobody");
+  };
 }
 
 function getRandomInt(max) {
